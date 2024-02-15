d39b5989ee7e0d0b9af1d78cc6c4eff3f67296a3
mishka49 <andreym3ch@gmail.com>
2024-02-15 16:38:09 +0300

    Initial

---
+++ WarehouseApp/WarehouseApp/Controllers/DepartmentController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WarehouseApp.UI.Models;
+
+namespace WarehouseApp.UI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllDepartments()
+        {
+            var departments = new List<DepartmentModel>
+            {
+                new DepartmentModel{Id = 1, Name = "Tools"},
+                new DepartmentModel{Id = 2, Name = "Industrial Wood"},
+                new DepartmentModel{Id= 3, Name = "Metal"}
+            };
+
+
+            return Ok(departments);
+        }
+
+    }
+}
